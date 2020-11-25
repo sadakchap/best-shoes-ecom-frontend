04c09aa82fcf351180442749e54647832791b4c6
sadakchap <aliceprerna@gmail.com>
2020-11-25 10:08:06 +0530

    nivo graph updated

--- src/components/user/AdminDashboard/index.js
+++ src/components/user/AdminDashboard/index.js
@@ -41,17 +41,6 @@ const EarningsChart = ({ data }) => (
         pointBorderWidth={4}
         pointBorderColor={{ from: 'serieColor' }}
         enableArea={true}
-        axisTop={null}
-        axisRight={null}
-        axisBottom={{
-            orient: 'bottom',
-            tickSize: 5,
-            tickPadding: 5,
-            tickRotation: 0,
-            legend: 'Date',
-            legendOffset: 36,
-            legendPosition: 'middle'
-        }}
         theme={{
           textColor: "#ccc"
         }}
@@ -59,12 +48,8 @@ const EarningsChart = ({ data }) => (
         axisLeft={{
             tickSize: 5,
             legendOffset: -40,
-            orient: 'left',
-            tickPadding: 5,
-            tickRotation: 0,
-            legend: 'sales',
-            legendPosition: 'middle'
         }}
+        useMesh={true}
     />
 )
 
