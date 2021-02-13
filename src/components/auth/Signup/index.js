32656050d9f7de9f81d2c1126ce44b879bc16ddb
sadakchap <aliceprerna@gmail.com>
2021-02-13 19:58:06 +0530

    added note on signup page to use dummy email

--- src/components/auth/Signup/index.js
+++ src/components/auth/Signup/index.js
@@ -48,47 +48,97 @@ const Signup = () => {
             setValues({ ...values, isComplete: false });
             return toast.error('Sorry, something went wrong!');
         });
-
-
     }
 
     return (
-        <Base>
-            {(isAuth() && isAuth().user.role === 1) ? (<Redirect to="/admin/dashboard" />) : null }
-            {(isAuth()) ? (<Redirect to="/user/dashboard" />) : null }
-            <ToastContainer />
-            <FormWrapper>
-                <FormHeaderText>Join Us!</FormHeaderText>
-                <StyledForm>
-                    <InputWrapper>
-                        <StyledInput type="text" placeholder="Name" onChange={handleChange("name")} value={name} style={{ textTransform: 'capitalize'}} />
-                    </InputWrapper>
-                    <InputWrapper>
-                        <StyledInput type="email" placeholder="Email" onChange={handleChange("email")} value={email} />
-                    </InputWrapper>
-                    <InputWrapper>
-                        <StyledInput type="password" placeholder="Password" onChange={handleChange("password")} value={password} />
-                    </InputWrapper>
-                    <InputWrapper>
-                        <StyledInput type="password" placeholder="Password Confirm" onChange={handleChange("passwordConfirm")} value={passwordConfirm} />
-                    </InputWrapper>
-                    <br/>
-                    <PrimaryButton width="180px" type="submit" onClick={handleSubmit} disabled={isLoading || isComplete}>
-                        {!isComplete && "Sign up"}
-                        {isComplete && (
-                            <>
-                                <FiCheck size="1.5rem" style={{ marginRight: '0.6em'}} />
-                                Finished
-                            </>
-                        )}
-                        <SpinnerWrapper isClicked={isLoading}>
-                            <Spinner />
-                        </SpinnerWrapper>
-                    </PrimaryButton>
-                </StyledForm>
-            </FormWrapper>
-        </Base>
-    )
+      <Base>
+        {isAuth() && isAuth().user.role === 1 ? (
+          <Redirect to="/admin/dashboard" />
+        ) : null}
+        {isAuth() ? <Redirect to="/user/dashboard" /> : null}
+        <ToastContainer />
+        <FormWrapper>
+          <FormHeaderText>Join Us!</FormHeaderText>
+          <StyledForm>
+            <InputWrapper>
+              <StyledInput
+                type="text"
+                placeholder="Name"
+                onChange={handleChange("name")}
+                value={name}
+                style={{ textTransform: "capitalize" }}
+              />
+            </InputWrapper>
+            <InputWrapper>
+              <StyledInput
+                type="email"
+                placeholder="Email"
+                onChange={handleChange("email")}
+                value={email}
+              />
+            </InputWrapper>
+            <InputWrapper>
+              <StyledInput
+                type="password"
+                placeholder="Password"
+                onChange={handleChange("password")}
+                value={password}
+              />
+            </InputWrapper>
+            <InputWrapper>
+              <StyledInput
+                type="password"
+                placeholder="Password Confirm"
+                onChange={handleChange("passwordConfirm")}
+                value={passwordConfirm}
+              />
+            </InputWrapper>
+            <br />
+            <PrimaryButton
+              width="180px"
+              type="submit"
+              onClick={handleSubmit}
+              disabled={isLoading || isComplete}
+            >
+              {!isComplete && "Sign up"}
+              {isComplete && (
+                <>
+                  <FiCheck size="1.5rem" style={{ marginRight: "0.6em" }} />
+                  Finished
+                </>
+              )}
+              <SpinnerWrapper isClicked={isLoading}>
+                <Spinner />
+              </SpinnerWrapper>
+            </PrimaryButton>
+            <p
+              style={{
+                color: "#a5a2a2",
+                textAlign: "left",
+                fontSize: "0.9rem",
+                marginTop: "2em",
+                lineHeight: "0.9rem",
+                letterSpacing: "0.5px"
+              }}
+            >
+              <small>
+                Note: Please use a dummy email address, if you don't have one,
+                you can easily get one using{" "}
+                <a
+                  style={{ color: "#ff7e39" }}
+                  href="https://temp-mail.org/en/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  tempmail.org
+                </a>
+                !
+              </small>
+            </p>
+          </StyledForm>
+        </FormWrapper>
+      </Base>
+    );
 }
 
 export default Signup
